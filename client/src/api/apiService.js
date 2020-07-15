8dd5d86843794293ef52703d9638725eab1d0c1e
Mauricio Viana <mau_viana@live.com>
2020-07-15 15:25:29 -0300

    Adicionado arquivo para comunicar com o backend

---
+++ client/src/api/apiService.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3001/api/transaction';
+
+async function getPeriodTransaction(period) {
+  const response = await axios.get(`${API_URL}?period=${period}`);
+  return response.data;
+}
+
+async function create(data) {
+  const response = await axios.post(API_URL, data);
+  return response.data;
+}
+
+async function update(data) {
+  const response = await axios.put(`${API_URL}?_id=${data.id}`, data);
+  return response.data;
+}
+
+async function remove(data) {
+  const response = await axios.delete(`${API_URL}?_id=${data.id}`);
+  return response.data;
+}
+
+export { getPeriodTransaction, create, update, remove };
