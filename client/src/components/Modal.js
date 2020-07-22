a0adce9fb89c72012717d71520100f7f5dbbd08b
Mauricio Viana <mau_viana@live.com>
2020-07-22 19:21:42 -0300

    Atualizado para receber novas props

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,27 +1,61 @@
 import React, { useState } from 'react';
 
-export default function Modal({ title, onSave }) {
-  const startDate = new Date();
-
-  const [formData, setFormData] = useState({});
-  const [date, setDate] = useState(
-    `${startDate.getFullYear()}-${startDate
-      .getMonth()
+export default function Modal({ title, onSave, data, isEdit }) {
+  const [formData, setFormData] = useState(data);
+  const [yearMonthDay, setYearMonthDay] = useState(
+    `${new Date().getFullYear()}-${(new Date().getMonth() + 1)
       .toString()
-      .padStart(2, '0')}-${startDate.getDate()}`
+      .padStart(2, '0')}-${new Date().getDate()}`
   );
+
+  const [isValid, setIsValid] = useState(false);
+
+  React.useEffect(() => {
+    if (Object.entries(data).length > 0) {
+      setFormData(data);
+      return;
+    }
+
+    const newFormData = {
+      value: 0,
+      category: '',
+      description: '',
+      type: '-',
+      yearMonthDay: `${new Date().getFullYear()}-${(new Date().getMonth() + 1)
+        .toString()
+        .padStart(2, '0')}-${new Date().getDate()}`,
+    };
+
+    setFormData(newFormData);
+    console.log(formData);
+  }, [data]);
+
+  const validate = () => {
+    const { description, category, value } = formData;
+    const validated =
+      description &&
+      description !== '' &&
+      category &&
+      category !== '' &&
+      value > 0;
+    console.log(description);
+    setIsValid(validated);
+  };
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    onSave(formData);
+    console.log('passei no submit');
+    if (!isValid) return;
+
+    onSave(formData, isEdit);
   };
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    console.log(event.target);
-    if (name === 'date') setDate(value);
+    if (name === 'yearMonthDay') setYearMonthDay(value);
     setFormData({ ...formData, [name]: value });
-    console.log(formData);
+    console.log('passei no change');
+    validate();
   };
 
   return (
@@ -30,7 +64,7 @@ export default function Modal({ title, onSave }) {
       className="modal modal-fixed-footer"
       style={{ maxWidth: '500px', maxHeight: '550px' }}
     >
-      <form onSubmit={handleFormSubmit}>
+      <form action="handleFormSubmit" onSubmit={handleFormSubmit}>
         <div className="modal-content">
           <div
             style={{
@@ -55,6 +89,7 @@ export default function Modal({ title, onSave }) {
                     type="radio"
                     value="+"
                     onChange={handleInputChange}
+                    defaultChecked={formData && formData.type === '+'}
                   />
                   <span>Despesa</span>
                 </label>
@@ -66,6 +101,7 @@ export default function Modal({ title, onSave }) {
                     type="radio"
                     value="-"
                     onChange={handleInputChange}
+                    defaultChecked={formData && formData.type === '-'}
                   />
                   <span>Receita</span>
                 </label>
@@ -79,6 +115,8 @@ export default function Modal({ title, onSave }) {
               name="description"
               type="text"
               className="validate"
+              required
+              defaultValue={formData && formData.description}
               onChange={handleInputChange}
             />
           </div>
@@ -89,6 +127,8 @@ export default function Modal({ title, onSave }) {
               name="category"
               type="text"
               className="validate"
+              required
+              defaultValue={formData && formData.category}
               onChange={handleInputChange}
             />
           </div>
@@ -104,24 +144,31 @@ export default function Modal({ title, onSave }) {
                 min="0"
                 step="0.01"
                 className="validate"
+                required
+                defaultValue={formData && formData.value}
                 onChange={handleInputChange}
               />
             </div>
             <div className="col s6" style={{ paddingRight: '0px' }}>
-              <label htmlFor="date">Data</label>
+              <label htmlFor="yearMonthDay">Data</label>
               <input
-                id="date"
-                name="date"
+                id="yearMonthDay"
+                name="yearMonthDay"
                 type="date"
                 className="validate"
-                value={date}
+                required
+                defaultValue={formData && formData.yearMonthDay}
                 onChange={handleInputChange}
               />
             </div>
           </div>
         </div>
         <div className="modal-footer">
-          <button type="submit" className="waves-effect waves-light btn-small">
+          <button
+            type="submit"
+            className="waves-effect waves-light btn-small"
+            disabled={!isValid}
+          >
             Salvar
           </button>
         </div>
