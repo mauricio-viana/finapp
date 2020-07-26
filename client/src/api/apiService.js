6de3e4f71ec57fd89ceb023ddf6155bb0de174c2
Mauricio Viana <mau_viana@live.com>
2020-07-26 10:46:32 -0300

    Criado rota base com axios

--- client/src/api/apiService.js
+++ client/src/api/apiService.js
@@ -1,24 +1,26 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api/transaction';
+const url = axios.create({
+  baseURL: '/api/transaction',
+});
 
 async function getPeriodTransaction(period) {
-  const response = await axios.get(`${API_URL}?period=${period}`);
+  const response = await url.get(`?period=${period}`);
   return response.data;
 }
 
 async function create(data) {
-  const response = await axios.post(API_URL, data);
+  const response = await url.post('', data);
   return response.data;
 }
 
 async function update(data) {
-  const response = await axios.put(`${API_URL}?_id=${data._id}`, data);
+  const response = await url.put(`?_id=${data._id}`, data);
   return response.data;
 }
 
 async function remove(data) {
-  const response = await axios.delete(`${API_URL}?_id=${data._id}`);
+  const response = await url.delete(`?_id=${data._id}`);
   return response.data;
 }
 
